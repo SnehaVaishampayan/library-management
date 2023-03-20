735f3cc05b66825a2dca993926df3377e7e174ac
SnehaGitUser <sneha_gadre@yahoo.com>
2023-03-19 23:41:34 -0700

    Adding init data to database

--- database/CreateDatabase.py
+++ database/CreateDatabase.py
@@ -1,6 +1,8 @@
 import sqlite3
+import uuid
+from datetime import datetime
 
-from Constants import DROP_TABLE, CREATE_TABLE_QUERY
+from Constants import DROP_TABLE, CREATE_TABLE_QUERY, CREATE_BOOK_QUERY
 
 global conn
 
@@ -19,6 +21,25 @@ def create_db_table():
         conn = sqlite3.connect(r"./database/database.db")
         conn.execute(DROP_TABLE)
         conn.execute(CREATE_TABLE_QUERY)
+        init_book_1 = {}
+        init_book_1['id'] = str(uuid.uuid4())
+        init_book_1['created'] = datetime.now()
+        init_book_1['updated'] = datetime.now()
+        conn.execute(CREATE_BOOK_QUERY, (
+            init_book_1['id'], 'test_book_name_1',
+            'test_description_1', init_book_1['created'],
+            init_book_1['updated'], "to do", 'test_author_1',
+            'test_publication_1','10'))
+        init_book_2 = {}
+        init_book_2['id'] = str(uuid.uuid4())
+        init_book_2['created'] = datetime.now()
+        init_book_2['updated'] = datetime.now()
+        conn.execute(CREATE_BOOK_QUERY, (
+            init_book_2['id'], 'test_book_name_2',
+            'test_description_2', init_book_2['created'],
+            init_book_2['updated'], "to do", 'test_author_2',
+            'test_publication_2','10'))
+
         conn.commit()
         print("Book table created successfully")
         conn.close()
